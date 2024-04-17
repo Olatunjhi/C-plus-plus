049c921b27d157be4e9040b940b19668824e2738
olatunjhi <kinghorlatunjhi.com>
2024-04-17 11:21:18 +0100

    add another code

---
+++ structure-and-function.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+struct firstSemeter
+{
+	int mathScore;
+	int englishScore;
+	int physicsScore;
+};
+
+void displayData(firstSemeter student1)
+{
+	cout << "Student1 math score is: " << student1.mathScore << endl;
+	cout << "stundent1 english score is: " << student1.englishScore << endl;
+	cout << "student1 physics score is: " << student1.physicsScore << endl;
+}
+
+void collectData()
+{
+	firstSemeter student1;
+
+	cout << "Enter Math score for student1: ";
+	cin >> student1.mathScore;
+
+	cout << "Enter English score for student1: ";
+	cin >> student1.englishScore;
+
+	cout << "Entet Physics score for student1: ";
+	cin >> student1.physicsScore;
+
+	displayData(student1);
+}
+
+int main()
+{
+	collectData();
+
+	return (0);
+}
